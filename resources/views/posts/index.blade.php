faaac8fb6622e901592d5520d54ba01e92a34cdd
rokan <rokansubhi.f@gmail.com>
2023-03-07 11:35:06 +0700

    DASAR

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,8 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   </head>
   <body>
-    @foreach($data_toko as $dt)
-    $num = 1
+    
   <table class="table">
   <thead>
     <tr>
@@ -21,7 +20,7 @@
   <tbody>
     <tr>
       <th scope="row">$num</th>
-      <td>{{ $data_toko[1] }}</td>
+      <td></td>
       <td>Otto</td>
       <td>@mdo</td>
     </tr>
@@ -38,8 +37,6 @@
     </tr>
   </tbody>
 </table>
-$num++
-@endforeach
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
   </body>
 </html>
